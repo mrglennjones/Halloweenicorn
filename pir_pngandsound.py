fadeee115f6860591308f2e3dc5b2764c3f9432b
mrglennjones <78789353+mrglennjones@users.noreply.github.com>
2023-10-04 17:09:32 +0100

    Update pir_pngandsound.py
    added four new faces

--- pir_pngandsound.py
+++ pir_pngandsound.py
@@ -1,6 +1,3 @@
-#Halloweenicorn v.1 betarrr - @mrglennjones - Thanks to my good friend pb and hel from Pimoroni for the ninja style help.
-#apololgies to my mum for not commenting the code very well.
-
 import time
 import ntptime
 import urequests
@@ -16,6 +13,7 @@ graphics = PicoGraphics(display=DISPLAY_COSMIC_UNICORN)
 gu = CosmicUnicorn()
 
 #Setup PIR Sensor
+#PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
 pir =  machine.Pin(5)
 
 
@@ -23,14 +21,15 @@ gu.set_brightness(0.2)
 
 wp = WavePlayer(gu)
 
-#PNG spritesheet
 png = PNG(graphics)
+#png.open_file("/s4m_ur4i-pirate-characters.png")
 png.open_file("/faces.png")
 
+gu.set_volume(0.1) 
 LAUGH_SOUND = [ 'laugh/%s'%f for f in os.listdir('laugh') ]
 print (LAUGH_SOUND)
 
-#set the frame size for the animations
+
 frame_width = 32
 frame_height = 32
 
@@ -71,8 +70,8 @@ draculaframes.append(frame (2, 2))
 draculaframes.append(frame (3, 2))
 draculaframes.append(frame (2, 2))
 draculaframes.append(frame (3, 2))
-draculaframes.append(frame (1, 2))
 draculaframes.append(frame (0, 2))
+draculaframes.append(frame (1, 2))
 
 ghostframes = []
 # describe as index from 0 - 3
@@ -85,7 +84,51 @@ ghostframes.append(frame (1, 3))
 ghostframes.append(frame (2, 3))
 ghostframes.append(frame (3, 3))
 
-faceframes =["skullframes", "pumpkinframes", "draculaframes", "ghostframes"]
+wolfframes = []
+# describe as index from 0 - 3
+wolfframes.append(frame (0, 4))
+wolfframes.append(frame (1, 4))
+wolfframes.append(frame (2, 4))
+wolfframes.append(frame (3, 4))
+wolfframes.append(frame (0, 4))
+wolfframes.append(frame (1, 4))
+wolfframes.append(frame (2, 4))
+wolfframes.append(frame (3, 4))
+
+frankframes = []
+# describe as index from 0 - 3
+frankframes.append(frame (0, 5))
+frankframes.append(frame (1, 5))
+frankframes.append(frame (2, 5))
+frankframes.append(frame (3, 5))
+frankframes.append(frame (0, 5))
+frankframes.append(frame (1, 5))
+frankframes.append(frame (2, 5))
+frankframes.append(frame (3, 5))
+
+mummyframes = []
+# describe as index from 0 - 3
+mummyframes.append(frame (0, 6))
+mummyframes.append(frame (1, 6))
+mummyframes.append(frame (2, 6))
+mummyframes.append(frame (3, 6))
+mummyframes.append(frame (0, 6))
+mummyframes.append(frame (1, 6))
+mummyframes.append(frame (2, 6))
+mummyframes.append(frame (3, 6))
+
+witchframes = []
+# describe as index from 0 - 3
+witchframes.append(frame (0, 7))
+witchframes.append(frame (1, 7))
+witchframes.append(frame (2, 7))
+witchframes.append(frame (3, 7))
+witchframes.append(frame (0, 7))
+witchframes.append(frame (1, 7))
+witchframes.append(frame (2, 7))
+witchframes.append(frame (3, 7))
+
+faceframes =["skullframes", "pumpkinframes", "draculaframes", "ghostframes", "wolfframes", "frankframes", "mummyframes", "witchframes"]
 
 selected_frame = random.choice(faceframes)
 print(f"Selected frame: {selected_frame}")
@@ -96,7 +139,7 @@ def JumpScare():
     global busy
     
     busy = True
-     
+    gu.set_volume(0.1) 
     #play random sound
     #wp.play('laugh/LAUGH-1.wav', loop=1)
     wp.play(random.choice(LAUGH_SOUND), loop=1)
@@ -113,20 +156,29 @@ def JumpScare():
         frame_list = draculaframes
     elif selected_frame == "ghostframes":
         frame_list = ghostframes
+    elif selected_frame == "wolfframes":
+        frame_list = wolfframes
+    elif selected_frame == "frankframes":
+        frame_list = frankframes
+    elif selected_frame == "mummyframes":
+        frame_list = mummyframes
+    elif selected_frame == "witchframes":
+        frame_list = witchframes
     else:
         frame_list = []  # Handle the case when selected_frame is invalid
 
 
     for frame in frame_list:
+        gu.set_brightness(0.2)
         # Clear the display before drawing the new frame
         graphics.set_pen(black)
         graphics.clear()
 
         # Decode and display the current frame
         png.decode(0, 0, source=(frame.frame_x * frame_width, frame.frame_y * frame_height, frame_width, frame_height), scale=(1, 1), rotate=0)
-        
-        gu.update(graphics)
-
+        #png.decode(0, 0, source=(frame.frame_x * frame_width, frame.frame_y * frame_height, frame_width, frame_height), scale=(1, 1), rotate=0)
+        #gu.adjust_brightness(-0.25)  # brightness ad
+        gu.update(graphics)        
         time.sleep(0.1)  # Adjust speed of anim
 
     # Clear display
@@ -147,4 +199,5 @@ while True:
         
 
     
+
 
